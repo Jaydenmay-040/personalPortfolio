02f9c2be18ca67f39a3b42c4e97c9ef5fa4c8d9a
Jaydenmay-040 <82215016+Jaydenmay-040@users.noreply.github.com>
2022-01-20 15:07:15 +0200

    new portfolio

--- SRC/script.js
+++ SRC/script.js
@@ -1,3 +1,28 @@
+// LOADER FUNCTION
 window.addEventListener("load", () => {
-  document.querySelector(".loaderContainer").classList.add("hide");
+    document.querySelector(".loaderContainer").classList.add("hide");
+});
+
+// HOME IMGS FUNCTION
+var object1 = $('#img2');
+var object2 = $('#img4');
+
+var layer = $('#main');
+
+layer.mousemove(function(e) {
+    var valueX = (e.pageX * -1 / 20);
+    var valueY = (e.pageY * -1 / 20);
+
+    object1.css({
+        'transform':'translate(-50%, -50%) translate3d('+valueX+'px,'+valueY+'px,0)'
+    });
+});
+
+layer.mousemove(function(e) {
+    var valueX = (e.pageX * -1 / 50);
+    var valueY = (e.pageY * -1 / 50);
+
+    object2.css({
+        'transform':'translate(-50%, -50%) translate3d('+valueX+'px,'+valueY+'px,0)'
+    });
 });
