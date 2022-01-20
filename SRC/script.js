6b6652cffd28d228ddb595cf7c1329eb047c2c7d
Jaydenmay-040 <82215016+Jaydenmay-040@users.noreply.github.com>
2022-01-20 16:31:16 +0200

    updates on changes

--- SRC/script.js
+++ SRC/script.js
@@ -7,7 +7,7 @@ window.addEventListener("load", () => {
 var object1 = $('#img2');
 var object2 = $('#img4');
 
-var layer = $('#main');
+var layer = $('#home');
 
 layer.mousemove(function(e) {
     var valueX = (e.pageX * -1 / 20);
@@ -26,3 +26,11 @@ layer.mousemove(function(e) {
         'transform':'translate(-50%, -50%) translate3d('+valueX+'px,'+valueY+'px,0)'
     });
 });
+
+function toggle_visibility(id) {
+    var e = document.getElementById(id);
+    if(e.style.display == 'block')
+       e.style.display = 'none';
+    else
+       e.style.display = 'block';
+ }
