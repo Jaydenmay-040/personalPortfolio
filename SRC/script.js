f5fe71021eda07feb7b82fccb84eddd79b32da40
Jaydenmay-040 <82215016+Jaydenmay-040@users.noreply.github.com>
2022-01-24 15:28:03 +0200

    updates on about

--- SRC/script.js
+++ SRC/script.js
@@ -27,6 +27,30 @@ layer.mousemove(function(e) {
     });
 });
 
+// HOME IMGS FUNCTION
+var object1 = $('#img2');
+var object2 = $('#img4');
+
+var layer = $('#about');
+
+layer.mousemove(function(e) {
+    var valueX = (e.pageX * -1 / 20);
+    var valueY = (e.pageY * -1 / 20);
+
+    object1.css({
+        'transform':'translate(-50%, -50%) translate3d('+valueX+'px,'+valueY+'px,0)'
+    });
+});
+
+layer.mousemove(function(e) {
+    var valueX = (e.pageX * -1 / 50);
+    var valueY = (e.pageY * -1 / 50);
+
+    object2.css({
+        'transform':'translate(-50%, -50%) translate3d('+valueX+'px,'+valueY+'px,0)'
+    });
+});
+
 // CURSOR FUNCTIONALITY
 let innerCursor = document.querySelector(".inner-cursor");
 let outerCursor = document.querySelector(".outer-cursor");
